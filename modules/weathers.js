bcc30ce473eca38e85e9eb85961188d9f1f6b90a
n-szilard <nagyapatiszilard@turr.hu>
2025-09-24 08:14:13 +0200

    Get all weathers by uid endpoint

--- modules/weathers.js
+++ modules/weathers.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { weathers, getNextWeatherID, saveWeather } = require('../utils/store');
+const { weathers, getNextWeatherID, saveWeather, users } = require('../utils/store');
 const router = express.Router();
 
 // GET all weathers
@@ -10,15 +10,31 @@ router.get('/', (req, res) => {
 // POST weather by uid
 router.post('/', (req, res) => {
     let data = req.body; // uid, min, max, szazalek, mm, datum, tipus
+    let folytat = true;
     weathers.forEach(weather => {
         if (weather.uid == data.uid && weather.datum == data.datum) {
-            return res.status(400).send('Ehhez a dátumhoz már van felvéve időjárás!');
+            folytat = false;
+            return res.status(400).send({msg: 'Ehhez a dátumhoz már van felvéve időjárás!'});
         }
     })
-    weathers.push(data);
-    data.id = getNextWeatherID();
-    saveWeather();
-    res.send({msg: 'Időjárás jelentés sikeresen felvéve!'})
-})
+    if (folytat) {
+        weathers.push(data);
+        data.id = getNextWeatherID();
+        saveWeather();
+        res.send({msg: 'Időjárás jelentés sikeresen felvéve!'})
+    }
+});
+
+// GET all weathers by uid
+router.get('/:id', (req, res) => {
+    id = Number(req.params.id);
+    let matchWeathers = [];
+    weathers.forEach(weather => {
+        if (weather.uid == id) {
+            matchWeathers.push(weather);
+        }
+    });
+    res.send(JSON.stringify(matchWeathers));
+});
 
 module.exports = router;
